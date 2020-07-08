d1a46d1017e6084aeab81b099007d67d379c07c8
Barry Rollan <barryrollan@macbook-pro.lan>
2020-07-08 13:01:19 -0400

    initial commit, but rather complete

---
+++ README.md
@@ -0,0 +1,1 @@
+This is a complete package for an encrypted/hashed sign-up- and sign-in-with-email-and-password router function using bcrypt, express, jsonwebtoken, mongoose and nodemon. Includes mongoDB set-up.
---
+++ src/index.js
@@ -0,0 +1,36 @@
+require('./models/User');
+const express = require('express');
+const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+const authRoutes = require('./routes/authRoutes');
+const requireAuth = require('./middlewares/requireAuth');
+
+const app = express();
+
+app.use(bodyParser.json());
+
+app.use(authRoutes);
+
+const mongoUri =
+  'mongodb+srv://admin:Powell3478!@cluster0.xeqjk.gcp.mongodb.net/<dbname>?retryWrites=true&w=majority';
+
+mongoose.connect(mongoUri, {
+  useNewUrlParser: true,
+  useCreateIndex: true,
+  useUnifiedTopology: true,
+});
+
+mongoose.connection.on('connected', () => {
+  console.log('Connected to mongoose instance');
+});
+mongoose.connection.on('error', (err) => {
+  console.error('Error connecting to mongo ', err);
+});
+
+app.get('/', requireAuth, (req, res) => {
+  res.send(`Your email: ${req.user.email}`);
+});
+
+app.listen(3000, () => {
+  console.log('Listening on port 3000');
+});
